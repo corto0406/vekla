18427bde46382d430ab8e96d63f84fcb6de332f7
corto0406 <132603219+corto0406@users.noreply.github.com>
2024-07-17 17:06:29 +0200

    create pages in sidebar

--- src/components/layouts/Sidebar.js
+++ src/components/layouts/Sidebar.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './Sidebar.css'; // Import CSS file for styling
 import logo from '../images/logo.jpg'; // Ensure the path to your image is correct
 import HelpCenter from '../helpCenter/HelpCenter'; // Adjust path based on actual file location
+import dashboard from '../icons/dashboard.svg';
+import mySkills from '../icons/mySkills.svg';
+import myTraining from '../icons/myTraining.svg';
+import knowledgeHub from '../icons/knowledgeHub.svg';
+import growthPlan from '../icons/growthPlan.svg';
 
 function Sidebar() {
   return (
@@ -10,11 +15,36 @@ function Sidebar() {
         <img src={logo} alt="Logo" className="logo" />
       </div>
       <ul className="category-list">
-        <li><a href="/dashboard">Dashboard</a></li>
-        <li><a href="/mySkills">My Skills</a></li>
-        <li><a href="/myTraining">My Training</a></li>
-        <li><a href="/knowledgeHub">Knowledge Hub</a></li>
-        <li><a href="/growthPlan">Growth Plan</a></li>
+        <li>
+          <a href="/dashboard">
+            <img src={dashboard} alt="Dashboard Icon" className="dashboard-picture" />
+            Dashboard
+          </a>
+        </li>
+        <li>
+          <a href="/mySkills">
+            <img src={mySkills} alt="My Skills Icon" className="mySkills-picture" />
+            My Skills
+          </a>
+        </li>
+        <li>
+          <a href="/myTraining">
+            <img src={myTraining} alt="My Training Icon" className="myTraining-picture" />
+            My Training
+          </a>
+        </li>
+        <li>
+          <a href="/knowledgeHub">
+            <img src={knowledgeHub} alt="Knowledge Hub Icon" className="knowledgeHub-picture" />
+            Knowledge Hub
+          </a>
+        </li>
+        <li>
+          <a href="/growthPlan">
+            <img src={growthPlan} alt="Growth Plan Icon" className="growthPlan-picture" />
+            Growth Plan
+          </a>
+        </li>
       </ul>
       <div className="help">
         <HelpCenter />
